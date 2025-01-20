d4a753b0bdf5966da506b9d4468906d27156bcab
Olivia Melnic <omelnic@myseneca.ca>
2025-01-20 16:41:29 -0500

    finished part d

--- lab3d.py
+++ lab3d.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
+'''Lab3d Free space on root function'''
 # Author ID: omelnic
 
-import os
+import subprocess
 
-def freespace():
-    output = os.popen("df -h | grep '/$' | awk '{print $4}'")
-    output = output[0].decode('utf-8').strip()
+def free_space():
+    # creates subproccess to get available space on the / directory
+    p = subprocess.Popen(["df -h | grep '/$' | awk '{print $4}'"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    fspace = p.communicate()
+    # Convert stdout to a string and strip off the newline characters
+    output = fspace[0].decode('utf-8').strip()
     return output
 
 
 if __name__ == '__main__':
-    print(freespace())
+    print(free_space())
